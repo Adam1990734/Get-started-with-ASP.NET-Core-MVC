f96c6ebf3c3475f2b97145c882f0039f7aea38f3
Adam1990734 <palmai.mini@gmail.com>
2026-09-20 17:20:12 +0200

    Add search

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,7 +1,8 @@
-
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Models;
+using System.Linq;
 
 public class MoviesController : Controller
 {
@@ -13,9 +14,31 @@ public class MoviesController : Controller
     }
 
     // GET: MOVIES
-    public async Task<IActionResult> Index()    
+    public async Task<IActionResult> Index(string movieGenre, string searchString)    
     {
-        return View(await _context.Movie.ToListAsync());
+        if (_context.Movie == null)
+            return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+        IQueryable<string> genreQuery = from m in _context.Movie
+                                        orderby m.Genre
+                                        select m.Genre;
+        var movies = from m in _context.Movie
+                     select m;
+        if (!string.IsNullOrEmpty(searchString))
+            movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+        if (!string.IsNullOrEmpty(movieGenre))
+            movies = movies.Where(x => x.Genre == movieGenre);
+        var movieGenreVM = new MovieGenreViewModel()
+        {
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+            Movies = await movies.ToListAsync()
+        };
+        return View(movieGenreVM);
+    }
+
+    [HttpPost]
+    public string Index(string searchString, bool notUsed)
+    {
+        return "From [HttpPost]Index: filter on " + searchString;
     }
 
     // GET: MOVIES/Details/5
